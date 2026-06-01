b2306026ef71e5ceb0dc90f5f6d6b12b884bd825
Eren <e.basaran2004@gmail.com>
2026-06-01 23:44:29 +0200

    initial commit

---
+++ backend/API/Program.cs
@@ -0,0 +1,25 @@
+using Application;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
